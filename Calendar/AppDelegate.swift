21c32f04116532651afda165db74d1d370cbdd78
Askhat Gumirov <agumirov.dev@gmail.com>
2023-09-09 23:18:27 +0600

    Initial Commit

---
+++ Calendar/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  Calendar
+//
+//  Created by G G on 09.09.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: - Properties
+    var window: UIWindow?
+
+    // MARK: - App lifecycle
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        startApp()
+        return true
+    }
+
+    // MARK: - Methods
+    private func startApp() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let navigationController = UINavigationController()
+        self.window?.rootViewController = navigationController
+        navigationController.viewControllers = [ViewController()]
+    }
+}
+
