096266570e1f7bc11b7c3495e972bef66d9c457f
Askhat Gumirov <agumirov.dev@gmail.com>
2023-09-10 01:42:11 +0600

    Business model added

---
+++ Calendar/Models/EventModel.swift
@@ -0,0 +1,15 @@
+//
+//  EventBusinessModel.swift
+//  Calendar
+//
+//  Created by G G on 10.09.2023.
+//
+
+import Foundation
+
+struct EventModel {
+    let eventId: UUID
+    let eventName: String
+    let eventDate: Date
+    let eventTime: Date
+}
